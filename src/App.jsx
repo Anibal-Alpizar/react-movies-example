b3dcfd646fa6e27c7531354b45daa1a5e24e14f9
anibal-alpizar <anibal.alpizar14@gmail.com>
2022-10-16 15:29:35 -0600

    using react-router-dom@5

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import MoviesGrid from "./MoviesGrid";
-import styles from './App.module.css'
+import MoviesGrid from "./components/MoviesGrid";
+import styles from "./components/App.module.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <header>
         <h1 className={styles.title}>Movies</h1>
       </header>
       <main>
-        <MoviesGrid />
+        <Switch>
+          <Route path="/movie">Movies</Route>
+          <Route path="/">Home</Route>
+        </Switch>
       </main>
-    </div>
+    </Router>
   );
 }
 
