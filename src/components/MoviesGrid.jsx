dd29c9678cb194362a0ae70156bba115f192cdef
anibal-alpizar <anibal.alpizar14@gmail.com>
2022-10-16 23:14:35 -0600

    added api for movies

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MovieCard from "./MovieCard";
 import movies from "./movies.json";
-import styles from './MoviesGrid.module.css'
+import styles from "./MoviesGrid.module.css";
 
 function MoviesGrid() {
+  useEffect(() => [
+    fetch("/discover/movie", {
+      headers: {
+        Authorization: "Bearer",
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    }),
+  ]);
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
